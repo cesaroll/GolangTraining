961129dd472fab0422317e9000bb4748549ab3f1
Cesar Lopez <cesarl@outlook.com>
2015-12-03 20:44:42 -0700

    Visibility

---
+++ 04_scope/01_package-scope/02_visibility/vis/name.go
@@ -0,0 +1,4 @@
+package vis
+
+var MyName = "Cesar"
+var yourName = "Rock Start Developer"
--- 04_scope/04_variable-shadowing/main.go
+++ 04_scope/04_variable-shadowing/main.go
@@ -3,13 +3,12 @@ package main
 import "fmt"
 
 func max(x int) int {
-    return x * 2
+	return x * 2
 }
 
 func main() {
-    
-    max := max(7) //We don;t do this; bad coding practive to shadow variables/functions
-    fmt.Println(max)
+
+	max := max(7) //We don;t do this; bad coding practive to shadow variables/functions
+	fmt.Println(max)
 
 }
-
